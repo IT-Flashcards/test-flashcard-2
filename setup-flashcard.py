7dc24eb3c82395b6972676be23a876dde1f00e93
vagrant <vagrant@example.com>
2024-04-14 17:09:50 +0200

    Revert "test"
    This reverts commit 43970264c7a668a55a493a00e4c95c8779280bac.

--- setup-flashcard.py
+++ setup-flashcard.py
@@ -9,14 +9,6 @@ script_dir = os.path.dirname(script_path)
 
 def open_vscode_and_run_ng(directory):
     subprocess.Popen(["C:\\Users\\vagrant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe", directory])
-
-    command = "cmd /C cd /d " + directory + " && npm install --force --yes --no-optional --no-audit && ng s -o"
-    process = subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout, stderr = process.communicate()
-
-    if process.returncode == 0:
-        print("Command executed successfully:\n", stdout.decode())
-    else:
-        print("An error occurred:\n", stderr.decode())
+    subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && npm install --force --yes && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 open_vscode_and_run_ng(script_dir)
