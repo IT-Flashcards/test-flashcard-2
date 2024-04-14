43970264c7a668a55a493a00e4c95c8779280bac
vagrant <vagrant@example.com>
2024-04-14 16:59:35 +0200

    test

--- setup-flashcard.py
+++ setup-flashcard.py
@@ -9,6 +9,14 @@ script_dir = os.path.dirname(script_path)
 
 def open_vscode_and_run_ng(directory):
     subprocess.Popen(["C:\\Users\\vagrant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe", directory])
-    subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && npm install --force --yes && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+    command = "cmd /C cd /d " + directory + " && npm install --force --yes --no-optional --no-audit && ng s -o"
+    process = subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = process.communicate()
+
+    if process.returncode == 0:
+        print("Command executed successfully:\n", stdout.decode())
+    else:
+        print("An error occurred:\n", stderr.decode())
 
 open_vscode_and_run_ng(script_dir)
