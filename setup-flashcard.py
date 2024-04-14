51badbafdf18537b89a204750346a9efa068be1c
Mateusz Poręba <mateusz.poreba@outlook.com>
2024-04-14 15:42:11 +0200

    add setup flashcard python script

---
+++ setup-flashcard.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+# print(script_dir)
+# print("C:\\Users\\vagrant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+
+
+def open_vscode_and_run_ng(directory):
+    subprocess.Popen(["C:\\Users\\vagrant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe", directory])
+    subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+open_vscode_and_run_ng(script_dir)
