22259c872fe564542f5a0592d1d99be068b5ce68
Mateusz Poręba <mateusz.poreba@outlook.com>
2024-04-21 15:53:08 +0200

    cmd switches from k to c

--- setup-flashcard.py
+++ setup-flashcard.py
@@ -9,11 +9,11 @@ def open_vscode_and_run_ng(directory):
     vscode_process = subprocess.Popen(["C:\\Users\\vagrant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe", directory])
 
     # Uruchamianie terminala z poleceniem npm i i czekanie na jego zakończenie
-    npm_process = subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && npm i"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    npm_process = subprocess.Popen(["cmd", "/C", "cd /d " + directory + " && npm i"], creationflags=subprocess.CREATE_NEW_CONSOLE)
     npm_process.wait()  # Czekaj na zakończenie instalacji pakietów npm
 
     # Po zakończeniu npm i, uruchamianie Angulara
-    ng_process = subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    ng_process = subprocess.Popen(["cmd", "/C", "cd /d " + directory + " && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
     # Zapisywanie PID do pliku
     with open("process_pids.txt", "w") as file:
